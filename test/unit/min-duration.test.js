e3b57ab9ee6ec670aabad365c794e4e06e8fa801
Claudio Romano <claudiocro@gmail.com>
2017-10-02 16:53:48 +0200

    test(minDuration): test catch if not wait

--- test/unit/min-duration.test.js
+++ test/unit/min-duration.test.js
@@ -19,6 +19,16 @@ describe('minDuration', () => {
     });
   });
 
+  it('return the reject if the request is slower', async () => {
+    const start = new Date().getTime();
+    const call = minDuration({ minTime: 100 })(delayedPromiseCall());
+    return call(150).catch((d) => {
+      expect(new Date().getTime() - start).to.be.above(150);
+      return d;
+    });
+  });
+
+
   it('resolves immediatly the request if slower', async () => {
     const start = new Date().getTime();
     const call = minDuration({ minTime: 100 })(delayedPromiseCall());
