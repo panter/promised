20c701fba47860535d13d2b3dd3740b0d9aa756f
Claudio Romano <claudiocro@gmail.com>
2017-10-02 18:29:38 +0200

    fix(queued): scope the queued to the function that is wrapped

--- src/queued.js
+++ src/queued.js
@@ -9,46 +9,49 @@ export class QueuedPromiseError extends Error {
 
 export default function queued() {
   let queue = [];
-  const processPromise = (isError, fn, processOnlyOnce) => (result) => {
-    const { resolve, reject } = queue.splice(0, 1)[0];
 
-    if (processOnlyOnce && queue.find(({ fn: pfn }) => pfn === fn)) {
-      reject(new QueuedPromiseError(result));
-    } else if (isError) {
-      reject(result);
-    } else {
-      resolve(result);
-    }
+  return ({ processOnlyOnce = false } = {}) => (fn) => {
+    const processPromise = isError => (result) => {
+      const { resolve, reject } = queue.splice(0, 1)[0];
 
-    if (queue.length > 0) {
-      const { callerContext, execute, args } = queue[0];
-
-      queue[0].running = true;
-      execute.apply(callerContext, args);
-    }
-  };
-
-  return ({ processOnlyOnce = false } = {}) => fn => function fnWrapper(...args) {
-    const callerContext = this;
-    return new Promise((resolve, reject) => {
-      function execute() {
-        fn.apply(callerContext, args)
-            .then(processPromise(false, fn, processOnlyOnce))
-            .catch(processPromise(true, fn, processOnlyOnce));
+      if (processOnlyOnce && queue.find(({ fn: pfn }) => pfn === fn)) {
+        reject(new QueuedPromiseError(result));
+      } else if (isError) {
+        reject(result);
+      } else {
+        resolve(result);
       }
 
-      const isFirstInQueue = queue.length === 0;
+      if (queue.length > 0) {
+        const { callerContext, execute, args } = queue[0];
 
-      if (!isFirstInQueue && processOnlyOnce) {
-        queue = queue.filter(({ fn: pfn, running }) => (running || pfn !== fn));
-      }
-
-      queue.push({ fn, callerContext, execute, args, resolve, reject, running: false });
-
-      if (isFirstInQueue) {
         queue[0].running = true;
         execute.apply(callerContext, args);
       }
-    });
+    };
+
+    return function fnWrapper(...args) {
+      const callerContext = this;
+      return new Promise((resolve, reject) => {
+        function execute() {
+          fn.apply(callerContext, args)
+            .then(processPromise(false))
+            .catch(processPromise(true));
+        }
+
+        const isFirstInQueue = queue.length === 0;
+
+        if (!isFirstInQueue && processOnlyOnce) {
+          queue = queue.filter(({ fn: pfn, running }) => (running || pfn !== fn));
+        }
+
+        queue.push({ fn, callerContext, execute, args, resolve, reject, running: false });
+
+        if (isFirstInQueue) {
+          queue[0].running = true;
+          execute.apply(callerContext, args);
+        }
+      });
+    };
   };
 }
