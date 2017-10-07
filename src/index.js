dd10f7fba0d7106847efbd6e03fbf66409fbae6e
Claudio Romano <claudiocro@gmail.com>
2017-10-07 15:35:16 +0200

    feat(mapArgs)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import debounce from './debounce';
 import last from './last';
 import queued from './queued';
 import delay from './delay';
-import wrap from './wrap';
 import mapResponse from './map-response';
+import mapArgs from './map-args';
 import retry from './retry';
 import waitFor from './wait-for';
 import flow from './flow';
@@ -13,7 +13,8 @@ export default {
   last,
   queued,
   delay,
-  wrap,
+  mapArgs,
+  mapResponse,
   retry,
   waitFor,
   flow,
